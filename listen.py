9e675dbdc4a8bd36c1364cafe052dcdb76de57d8
Kyle Husmann <kyle.husmann@gmail.com>
2024-09-22 20:48:58 -0700

    add some command options

--- listen.py
+++ listen.py
@@ -11,19 +11,11 @@ stop_event = threading.Event()
 
 
 def hexdump(data: bytes):
-    # Print the hex dump of the data, 16 bytes per line
     for i in range(0, len(data), 16):
-        # Slice 16 bytes from the data
         chunk = data[i:i+16]
-
-        # Convert to hex values
         hex_values = ' '.join(f'{byte:02x}' for byte in chunk)
-
-        # Convert to ASCII, replace non-printable characters with a dot
         ascii_values = ''.join(chr(byte) if 32 <= byte <=
                                126 else '.' for byte in chunk)
-
-        # Print the offset, hex values, and ASCII values
         print(f'{i:08x}  {hex_values:<48}  {ascii_values}\n')
 
 
@@ -45,7 +37,7 @@ def decode_packet(data: bytes):
         hexdump(data)
 
 
-def listen_for_data(sock: socket.socket):
+def listen_for_cmd(sock: socket.socket):
     while not stop_event.is_set():
         ready_to_read, _, _ = select.select([sock], [], [], 1.0)
 
@@ -69,41 +61,119 @@ def listen_for_data(sock: socket.socket):
             time.sleep(0.1)
 
 
-def command_prompt(sock: socket.socket):
+def listen_for_data(sock: socket.socket):
+    while not stop_event.is_set():
+        ready_to_read, _, _ = select.select([sock], [], [], 1.0)
+
+        if ready_to_read:
+            data = sock.recv(1024)
+            if not data:
+                break
+
+            print()
+            print("Received data:")
+            hexdump(data)
+
+            # Print command prompt again
+            print("\nEnter command: ", end='')
+
+        else:
+            time.sleep(0.1)
+
+
+def command_prompt(command_sock: socket.socket, data_sock: socket.socket):
     while not stop_event.is_set():
         command = input("Enter command: ")
-        if command.strip() == "aprs":
-            byte_string = b'\x7e\x02\x00\x00\x00\x00\x00\x00\x00\x00\x7e'
-            print(sock.send(byte_string))
-            print("Sent byte string:", byte_string.hex())
+        if command.strip() == "test1":
+            command_sock.send(b'\x7e\x02\x00\x00\x00\x00\x00\x00\x00\x00\x7e')
+        elif command.strip() == "test2":
+            data_sock.send(b'\xff\x01\x00\x01\x00\x02\x00\x04\x03')
+            data_sock.send(
+                b'\xff\x01\x00\x10\x00\x02\x00\x20\x14\x2e\x0a\xc7\xa2\xfe\x00\x64\xff\xff\xff\xff\x66\xef\x8b\x6c'
+            )
+            data_sock.send(b'\xff\x01\x00\x00\x00\x02\x00\x16')
+            data_sock.send(b'\xff\x01\x00\x01\x00\x02\x00\x06\x01')
+            data_sock.send(b'\xff\x01\x00\x00\x00\x02\x00\x0a')
+            data_sock.send(b'\xff\x01\x00\x01\x00\x02\x00\x2c\x01')
+            data_sock.send(b'\xff\x01\x00\x01\x00\x02\x00\x0d\x00')
+        elif command.strip() == "test3":
+            command_sock.send(b'\x7e\x02\x00\x00\x00\x00\x00\x00\x00\x00\x7e')
+            command_sock.send(b'\x7e\x02\x00\x00\x00\x00\x00\x00\x00\x00\x7e')
+            # data_sock.send(b'\xff\x01\x00\x00\x00\x02\x00\x16')
+            data_sock.send(
+                b'\xff\x01\x00\x19\x00\x02\x00\x0e\x03\x08\xb2\x41\xe0\x08\xb2\x41\xe0\x00\x00\x00\x00\x15\x00\x62\x6c\x61\x68\x00\x00\x00\x00\x00\x00'
+            )
+        elif command.strip() == 'test4':
+            command_sock.send(b'\x7e\x02\x00\x00\x00\x00\x00\x00\x00\x00\x7e')
+            time.sleep(0.1)
+            command_sock.send(
+                b'\xff\x01\x00\x19\x00\x02\x00\x0e\x03\x08\xb3\xc8\x80\x08\xb3\xc8\x80\x00\x00\x00\x00\x15\x00\x62\x69\x6e\x67\x00\x00\x00\x00\x00\x00'
+            )
+            command_sock.send(
+                b'\xff\x01\x00\x00\x00\x02\x00\x16'
+            )
+        elif command.strip() == "test5":
+            command_sock.send(b'\x7e\x02\x00\x00\x00\x00\x00\x00\x00\x00\x7e')
+            data_sock.send(
+                b'\xff\x01\x00\x19\x00\x02\x00\x0e\x03\x08\xb3\xc8\x80\x08\xb3\xc8\x80\x00\x00\x00\x00\x15\x00\x62\x69\x6e\x67\x00\x00\x00\x00\x00\x00'
+            )
+            data_sock.send(
+                b'\xff\x01\x00\x00\x00\x02\x00\x16'
+            )
+            data_sock.send(
+                b'\xff\x01\x00\x00\x00\x02\x00\x0c'
+            )
+            data_sock.send(
+                b'\xff\x01\x00\x14\x00\x02\x00\x0b\x3f\x01\xa6\x06\x58\x10\x3c\xa0\xa1\x00\x00\x20\x00\x00\x00\x00\x00\x00\x00\x00'
+            )
+
         else:
-            print("Unknown command. Available commands: aprs")
+            print("Unknown command.")
 
 
 def bind_rfcomm(target_device_mac: str):
-    channel = 1
-    sock: t.Optional[socket.socket] = None
+    data_channel = 3
+    command_channel = 1
+    data_sock: t.Optional[socket.socket] = None
+    command_sock: t.Optional[socket.socket] = None
 
     try:
-        sock = socket.socket(socket.AF_BLUETOOTH,
-                             socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-        sock.connect((target_device_mac, channel))
-        print(f"Connected to {target_device_mac} on RFCOMM channel {channel}.")
+        # Data socket
+        data_sock = socket.socket(
+            socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        data_sock.connect((target_device_mac, data_channel))
+        print(
+            f"Connected to {target_device_mac} on RFCOMM channel {data_channel} (data socket).")
+
+        # Command socket
+        command_sock = socket.socket(
+            socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        command_sock.connect((target_device_mac, command_channel))
+        print(
+            f"Connected to {target_device_mac} on RFCOMM channel {command_channel} (command socket).")
 
         # Start a thread for listening to incoming data
-        listener_thread = threading.Thread(
-            target=listen_for_data, args=(sock,), daemon=True)
-        listener_thread.start()
+        cmd_listener_thread = threading.Thread(
+            target=listen_for_cmd, args=(command_sock,), daemon=True
+        )
+        cmd_listener_thread.start()
+
+        data_listener_thread = threading.Thread(
+            target=listen_for_data, args=(data_sock,), daemon=True
+        )
+        data_listener_thread.start()
 
         # Start the command prompt in the main thread
-        command_prompt(sock)
+        command_prompt(command_sock, data_sock)
 
     except socket.error as e:
         print(f"Error: {e}")
     finally:
         stop_event.set()  # Signal threads to stop
-        if sock:
-            sock.close()
+        if data_sock:
+            data_sock.close()
+        if command_sock:
+            command_sock.close()
         print("Disconnected.")
 
 
